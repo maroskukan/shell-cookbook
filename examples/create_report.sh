f34a98e82c1d8410c0338df3090522c384236f05
Maros Kukan <maros.kukan@me.com>
2020-09-14 14:52:16 +0200

    Conditional output added

--- examples/create_report.sh
+++ examples/create_report.sh
@@ -19,9 +19,15 @@ folder="$3"
 # Check if folder already exists
 if [ ! -d "$folder" ]; then mkdir -p -- "$folder"; fi
 
+
 # Filters the source file
-grep -- "$containerId" "$records" > "$folder/${containerId}_report.csv"
+if grep -- "$containerId" "$records" > "$folder/${containerId}_report.csv"
+then
+    records=$(wc -l "$folder/${containerId}_report.csv" | cut -d' ' -f1)
+    printf "Report %s/%s_report.csv created with %s records.\n" "$folder" "$containerId" "$records"
+    exit 0
+else
+    printf "Container id %s was not found in %s\n" "$containerId" "$records"
+    exit 1
+fi
 
-records=$(wc -l "$folder/${containerId}_report.csv" | cut -d' ' -f1)
-
-printf "Report %s/%s created with %s records.\n" "$folder" "$containerId" "$records"
