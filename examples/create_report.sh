c64b890392dc71b4deba213c8ed4b064bf4a4b78
Maros Kukan <maros.kukan@me.com>
2020-09-14 13:58:11 +0200

    Argument verification

--- examples/create_report.sh
+++ examples/create_report.sh
@@ -3,14 +3,20 @@
 # Create a report file for a single shipping container
 # Author: Maros Kukan
 
+# Verify number of arguments
+if [ "$#" -ne 2 ]; then
+  printf "Usage: %s CONTAINER-ID OUTPUT-FOLDER\n" "$0"
+  exit 1
+fi
+
 # Read arguments from arguments
 containerId="$1"
 folder="$2"
 
 if [ ! -d "$folder" ]; then mkdir -p -- "$folder"; fi
 
-grep -- "$containerId" shipments.csv > "$folder/$containerId.csv"
+grep -- "$containerId" shipments.csv > "$folder/${containerId}_report.csv"
 
-records=$(wc -l "$folder/$containerId.csv" | cut -d' ' -f1)
+records=$(wc -l "$folder/${containerId}_report.csv" | cut -d' ' -f1)
 
-printf "Report %s/%s created with %s records.\n" $folder $containerId $records
+printf "Report %s/%s created with %s records.\n" "$folder" "$containerId" "$records"
