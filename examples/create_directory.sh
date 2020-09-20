a24645424b3a723f62e2e9cc65d3aa43e9ff8721
Maros Kukan <maros.kukan@me.com>
2020-09-20 22:04:48 +0200

    Initial commit

---
+++ examples/create_directory.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Sample create directory script
+# Author: Maros Kukan
+
+declare -l DIR
+echo -n "Please enter a directory to create: "
+read DIR
+
+if [[ -e $DIR ]];then
+   echo "$DIR already exists"
+   exit 1
+else
+   if [[ -w $PWD ]];then
+      mkdir $DIR
+   else
+      echo "You don't have permissions to $PWD"
+      exit 2
+   fi
+fi
