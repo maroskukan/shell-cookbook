fd9452e7d34ef8b4083fe016dc6aa207ec39016f
Maros Kukan <maros.kukan@me.com>
2020-09-28 15:53:36 +0200

    Initial commit

---
+++ examples/break_continue.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Demostrates usage of break and continue
+# Author: Maros Kukan
+
+# List files not Directories
+for file in $(ls); do
+    if [[ -d $file ]]; then
+        continue
+    fi
+    echo $file
+done 
+
+# In case of break when test condition evaluates to true the loop is exited
+for file in $(ls); do 
+    if [[ $file == 'README.md' ]]; then
+        echo "README.md Exists"
+        break
+    fi
+    echo "$file No match"
+done
