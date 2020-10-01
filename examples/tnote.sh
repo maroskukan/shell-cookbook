77c3bda7dba7fb7c7e869f0244aef1a02377dbe2
Maros Kukan <maros.kukan@me.com>
2020-10-01 12:12:28 +0200

    Initial commit

---
+++ examples/tnote.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Simple note-taking script
+# Author: Maros Kukan
+
+# Enable debug
+#set -x
+
+declare date=$(date +%Y-%m-%d)
+declare topic="$1"
+declare filename="${HOME}/${topic}notes.txt"
+declare note
+
+# Ask user for input
+read -r -p "Please enter your note: " note
+
+# Disable debug
+#set +x
+
+if [[ $note ]]; then
+    # Prints date and retrieves all arguments and writes to file 
+    echo "${date}: $note" >> "$filename"
+    echo "Note '$note' saved to $filename"
+    exit 0
+else
+    echo "No input; note was't saved."
+    exit 1
+fi
