1be896845c26d0f321edcdd5eb0878a97771c8e2
Maros Kukan <maros.kukan@me.com>
2020-10-13 15:08:32 +0200

    EOF Processing added

--- bin/dog.sh
+++ bin/dog.sh
@@ -3,6 +3,7 @@
 # Description: An alternative to cat command
 # Author: Maros Kukan
 
+# Compound command to process EOF line
 while read -r || [[ $REPLY ]]; do
     printf "%s\n" "$REPLY"
 done <"$1"
