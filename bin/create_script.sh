26058dae576364a3fc34786054c0ae3505b625e0
Maros Kukan <maros.kukan@me.com>
2020-10-16 14:24:22 +0200

    Expanding to support multiple arguments with shift builtin

--- bin/create_script.sh
+++ bin/create_script.sh
@@ -5,46 +5,64 @@
 
 # Test if argument exists, if not exit
 if [[ ! $1 ]]; then
-    echo "Missing argument"
+    echo "Missing argument" >&2
     exit 1
 fi
 
-declare scriptname="$1"
-declare bindir="${HOME}/bin"
-declare filename="${bindir}/${scriptname}"
+declare scriptname
+declare -r bindir="${HOME}/bin"
+declare filename
+declare open_editor=""
 
-
-# Test if file already exists
-if [[ -e "$filename" ]]; then
-    echo "File ${filename} already exists"
-    exit 1
-fi
-
-if type "$scriptname" > /dev/null 2>&1; then
-    echo "There is already a command with name ${scriptname}"
-    exit 1
+# Test for single argument provided
+if [[ $# -eq 1 ]]; then
+    open_editor="true"
 fi
 
 # Test if bin directory exists
 if [[ ! -d "$bindir" ]]; then
     # if not: create it
     if mkdir "$bindir"; then
-        echo "Created ${bindir}"
+        echo "Created ${bindir}" >&2
     else
-        echo "Could not create ${bindir}."
+        echo "Could not create ${bindir}." >&2
         exit 1
     fi
 fi
 
-# Create a script with a single line
-echo '#!/usr/bin/env bash' > "$filename"
-# Add executable permission
-chmod u+x "$filename"
+while [[ $1 ]]; do
+    scriptname="$1"
+    filename="${bindir}/${scriptname}"
+
+    # Test if file already exists
+    if [[ -e "$filename" ]]; then
+        echo "File ${filename} already exists" >&2
+        shift
+        continue
+    fi
+
+    if type "$scriptname" > /dev/null 2>&1; then
+        echo "There is already a command with name ${scriptname}" >&2
+        shift
+        continue
+    fi
+
+    # Create a script with a single line
+    echo '#!/usr/bin/env bash' > "$filename"
+    echo "Created $filename"
+    # Add executable permission
+    chmod u+x "$filename"
+
+    shift
+done
 
 # Open in default editor
-if [[ $EDITOR ]]; then
-    $EDITOR "$filename"
-else
-    echo "Script created, not starting editor becasue \$EDITOR is not set."
+if [[ $open_editor ]]; then
+    echo "Opening"
+    if [[ $EDITOR ]]; then
+        $EDITOR "$filename"
+    else
+        echo "Script created, not starting editor becasue \$EDITOR is not set."
+    fi
 fi
-
+exit 0
