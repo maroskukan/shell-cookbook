37c9a977b827083f87d2a4b6688b2e8767330dd5
Maros Kukan <maros.kukan@me.com>
2020-11-02 16:48:15 +0100

    Version description updated

--- bin/chext.sh
+++ bin/chext.sh
@@ -6,14 +6,14 @@
 
 [[ $# -ne 2 ]] && { echo "Two arguments required, example: $0 txt zip" >&2; exit 1; }
 
-# Version 1.0
+# Version 1.0 - uses external base utility, which spawns a process for each file.
 # for file in *"$1"; do # *"$1" is a wildcard matching all files with given extension
 #     base=$(basename "$file" "$1")
 #     echo "$file > ${base}$2"
 #     mv "$file" "${base}$2" 
 # done
 
-# Version 1.1
+# Version 1.1 - uses builtin bash parameter extension
 for file in *"$1"; do
     echo "$file > ${file/%$1/$2}"
     mv "$file" "${file/%$1/$2}"
