c58ae51b07cee20ad5dabf0b0401f6c985bd60e1
Maros Kukan <maros.kukan@me.com>
2020-11-02 16:46:09 +0100

    Search and Replace added.

--- bin/chext.sh
+++ bin/chext.sh
@@ -3,11 +3,20 @@
 # Description: Changes filename extension
 # Author: Maros Kukan
 
+
 [[ $# -ne 2 ]] && { echo "Two arguments required, example: $0 txt zip" >&2; exit 1; }
 
-for file in *"$1"; do # *"$1" is a wildcard matching all files with given extension
-    base=$(basename "$file" "$1")
-    echo "$file > ${base}$2"
-    mv "$file" "${base}$2" 
+# Version 1.0
+# for file in *"$1"; do # *"$1" is a wildcard matching all files with given extension
+#     base=$(basename "$file" "$1")
+#     echo "$file > ${base}$2"
+#     mv "$file" "${base}$2" 
+# done
+
+# Version 1.1
+for file in *"$1"; do
+    echo "$file > ${file/%$1/$2}"
+    mv "$file" "${file/%$1/$2}"
 done
 exit 0
+
