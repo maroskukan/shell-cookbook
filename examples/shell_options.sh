48b6a299ef16c4c81ff93fbdb1991dc1129ee96f
Maros Kukan <maros.kukan@me.com>
2020-11-05 14:04:27 +0100

    Set option added

--- examples/shell_options.sh
+++ examples/shell_options.sh
@@ -3,20 +3,20 @@
 # Descroption: Demostrates usage of Shell Options (shops)
 # Author: Maros Kukan
 
-# Display all shell options
+# Display all shell options using shopt
 shopt
 
-# Display single shell option
+# Display single shell option using shopt
 shopt autocd
 
-# Enabling option
+# Enabling option using shopt
 shopt -s autocd
 /etc
 
 shopt -s cdspell
 cd /ect
 
-# Disabling option
+# Disabling option using shopt
 shopt -u autocd
 shopt -u cdspell
 
@@ -26,3 +26,16 @@ shopt -u cdspell
 rbash # or bash -r
 shopt restricted_shell
 
+# Displaying shell options using set
+set -o
+set +o
+
+# Changing shell options using set
+ls /etc > file1
+ls /etc > file1
+# This option enables overwrite protection
+set -o noclobber
+ls /etc > file1
+
+# To overrule this option
+ls /etc >| file1
