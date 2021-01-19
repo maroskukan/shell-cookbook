5d2fb4f4a376e5a0910ad168e8c9cdd19a78e5ff
Maros Kukan <maros.kukan@me.com>
2021-01-19 19:16:34 +0100

    Added custom argument parser

---
+++ examples/parser.sh
@@ -0,0 +1,33 @@
+#!/usr/bib/env bash
+
+# Description: Processes command line arguments
+# Author: Dew Stokes @medium.com
+# URL: https://medium.com/@Drew_Stokes/bash-argument-parsing-54f3b81a6a8f
+
+PARAMS=""
+while (( "$#" )); do
+  case "$1" in
+    -a|--my-boolean-flag)
+      MY_FLAG=0
+      shift
+      ;;
+    -b|--my-flag-with-argument)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        MY_FLAG_ARG=$2
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    -*|--*=) 
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *)
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+  esac
+done
+eval set -- "$PARAMS"
