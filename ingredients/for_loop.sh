253bb774714751c6e4165d0571f5f4221786b017
Maros Kukan <maros.kukan@me.com>
2022-10-05 06:36:31 +0200

    Cleanup user management

--- ingredients/for_loop.sh
+++ ingredients/for_loop.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-# Demostrates the use of for loop
+# Description: Demostrates the use of for loop
 # Author: Maros Kukan
+
 
 # Classic Loop (this behaves differently in Bash vs Zsh)
 declare sentence="just a list of words"
 for word in $sentence; do
-    echo $word;
+    echo "$word";
 done
+
+
+# Classic For loop - one liner
+for f in $(ls); do stat -c "%n %F" "$f"; done
 
 
 # C-style Loop
@@ -18,15 +23,18 @@ done
 
 for ((i=5; i>0; i--)); do echo "$i"; done
 
+
 # Iterating an Array
 declare -a users=("bob" "joe" "sue")
-
 echo "Number of items in array is ${#users[*]}."
 for ((i=0; i<${#users[*]}; i++)); do
-    echo "Adding user ${users[$i]}."
-    #sudo useradd ${users[$i]};
+    echo "Creating user ${users[$i]}."
+    sudo useradd "${users[$i]}"
+    echo ChangeMe | sudo passwd --stdin "${users[$i]}" &>/dev/null
+    tail -n1 /etc/passwd
 done
 
-
-# Classic For loop
-for f in $(ls); do stat -c "%n %F" $f; done
+for ((i=0; i<${#users[*]}; i++)); do
+    echo "Removing user ${users[$i]}."
+    sudo userdel -r "${users[$i]}"
+done
