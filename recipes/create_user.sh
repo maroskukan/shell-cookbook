d5ed0fc8e6262e89fa11e8334d02328c0564e41f
Maros Kukan <maros.kukan@me.com>
2022-10-06 09:19:05 +0200

    Move actions to functions

--- recipes/create_user.sh
+++ recipes/create_user.sh
@@ -12,26 +12,36 @@ HELP_USAGE
   exit 1
 }
 
+function create_user() {
+  if getent passwd "$1" &>/dev/null; then
+    echo "The user $1 already exists on this system"
+    exit 2
+  fi
+  sudo useradd "$1"
+  getent passwd "$1" &>/dev/null && echo "User created successfully." || echo "Failed to create user."
+}
 
-if [ "$#" -lt 1 ]; then 
-  usage
-elif getent passwd "$1" &>/dev/null; then
-  echo "The user $1 already exists on this system"
-  exit 2
-else
+function set_password() {
   while ! [ -n "$USER_PASSWORD" ]
   do
     read -s -p "Enter a new password for the user $1: " USER_PASSWORD
     echo
   done
-  sudo useradd "$1"
   DISTRIB=$(awk -F= '/^ID_LIKE/{print $2}' /etc/os-release)
-  if [ "$DISTRIB" = "fedora" ]; then 
+  if [ "$DISTRIB" = "fedora" ]; then
     echo "$USER_PASSWORD" | sudo passwd "$1" --stdin
-  else  
+  else
     echo "$1:$USER_PASSWORD" | sudo chpasswd
   fi
-  getent passwd "$1" &>/dev/null && echo "User created successfully." || echo "Failed to create user."
+}
+
+if [ "$#" -lt 1 ]; then
+  usage
 fi
-  
+
+for u in "$@"
+do
+  create_user "$u"
+  set_password "$u"
+done
 
